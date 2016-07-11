76d51046a4f83099336d35dd02ccfb8c11be3a31
truewave <truewave@truewave-macbook-pro.local>
2016-07-11 14:07:25 -0400

    Initial Commit

---
+++ TestGithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGithub
+//
+//  Created by truewave on 2016-07-11.
+//  Copyright © 2016 TrueWaveApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestGithub/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestGithub
+//
+//  Created by truewave on 2016-07-11.
+//  Copyright © 2016 TrueWaveApp. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
