080bac904536907f15cb5d0e3127a8333a247d82
truewave <truewave@truewave-macbook-pro.local>
2016-07-11 14:17:49 -0400

    Add Car Class

---
+++ TestGithub/Car.m
@@ -0,0 +1,15 @@
+//
+//  Car.m
+//  TestGithub
+//
+//  Created by truewave on 2016-07-11.
+//  Copyright © 2016 TrueWaveApp. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+
+
+@end
